825f248f3023fa03dfb6941842ab6011c7b36b1e
R-Mohammed-Hasan <mohammed.hasan@fwsa.freshworks.com>
2021-11-24 18:48:25 +0530

    adding

--- login.js
+++ login.js
@@ -9,9 +9,9 @@ function get(){
 function image(){
   document.getElementById("images").src="html images/eye.svg";
   }
-function image2(){
-    document.getElementById("images").src="html images/eye-close.svg";
-  }
+// function image2(){
+//     document.getElementById("images").src="html images/eye-close.svg";
+//   }
 function show1(){
   let pass = document.getElementById("ab");
   if(pass.type === "password"){
@@ -26,8 +26,12 @@ function show(){
   let pass = document.getElementById("password");
   if(pass.type === "password"){
     pass.type = "text";
+    document.getElementById("images").src="html images/eye.svg";
+
   }
   else{
     pass.type = "password";
+    document.getElementById("images").src="html images/eye-close.svg";
+
   }
 }
