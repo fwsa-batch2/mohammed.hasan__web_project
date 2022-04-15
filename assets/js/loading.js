4a78ec35135094b21d36e466eba2bfdf54151de6
R-Mohammed-Hasan <rafeek.ahmed1968@gmail.com>
2022-04-15 20:28:33 +0530

    Added Loading view for Users

---
+++ assets/js/loading.js
@@ -0,0 +1,5 @@
+window.onload = () => {
+    console.log("ready");
+    document.querySelector(".load").style.display = "none";
+    document.querySelector(".allContent").style.filter = "blur(0px)";
+}
